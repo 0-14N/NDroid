838fc8c9d86cdd3c19b73275a7a0576d73d78438
0-14N <0x00000454@gmail.com>
2014-09-22 11:36:59 +0800

    add logic/arithmetic shift operation implementation for ARM instructions.

---
+++ DECAF_shared/DroidScope/NDroid/darm/ndroid_darm_utils.c
@@ -0,0 +1,107 @@
+/**
+ * author: Chenxiong (R0r5ch4ch) Qian
+ * date: 2014-9-22
+ */
+#include "ndroid_darm_utils.h"
+#include <assert.h>
+
+int darm_my_lsl(int value, int shift){
+	assert(shift >= 0);
+	if(shift == 0){
+		return value;
+	}else{
+		return (value << shift);
+	}
+}
+
+int darm_my_lsr(int value, int shift){
+	assert(shift >= 0);
+	if(shift == 0){
+		return value;
+	}else{
+		return ((unsigned)value >> shift);
+	}
+}
+
+int darm_my_asr(int value, int shift){
+	assert(shift >= 0);
+	if(shift == 0){
+		return value;
+	}else{
+		if(value < 0){
+			return value >> shift | ~(~0U >> shift);
+		}else{
+			return value >> shift;
+		}
+	}
+}
+
+int darm_my_ror(int value, int shift){
+	assert(shift >= 0);
+	if(shift == 0){
+		return value;
+	}else{
+		return ((value >> shift)|(value << (32 - shift)));
+	}
+}
+
+int darm_my_rrx(int value, int carry_in){
+	if(carry_in == 0){
+		return (value >> 1);
+	}else{
+		return ((value >> 1) | 0x80000000);
+	}
+}
+
+int darm_shift(int value, int type, int imm, int carry_in){
+	darm_sr_type myType = darm_decode_imm_shift(type, imm);
+	int myImm = (myType == SRType_RRX) ? 1 : imm;
+	int retValue = 0;
+
+	if(myImm == 0){
+		retValue = value;	
+	}else{
+		switch(myType){
+			case SRType_LSL:
+				retValue = darm_my_lsl(value, myImm);
+				break;
+			case SRType_LSR:
+				retValue = darm_my_lsr(value, myImm);
+				break;
+			case SRType_ASR:
+				retValue = darm_my_asr(value, myImm);
+				break;
+			case SRType_ROR:
+				retValue = darm_my_ror(value, myImm);
+				break;
+			case SRType_RRX:
+				retValue = darm_my_rrx(value, carry_in);
+				break;
+		}
+	}
+
+	return retValue;
+}
+
+darm_sr_type darm_decode_imm_shift(int type, int imm){
+	darm_sr_type retType = SRType_INVALID;
+	switch(type){
+		case 0b00:
+			retType = SRType_LSL;
+			break;
+		case 0b01:
+			retType = SRType_LSR;
+			break;
+		case 0b10:
+			retType = SRType_ASR;
+			break;
+		case 0b11:
+			if(imm == 0){
+				retType = SRType_RRX;
+			}else{
+				retType = SRType_ROR;
+			}
+			break;
+	}
+	return retType;
+}
---
+++ DECAF_shared/DroidScope/NDroid/darm/ndroid_darm_utils.h
@@ -0,0 +1,31 @@
+/**
+ * author: Chenxiong (R0r5ch4ch) Qian
+ * date: 2014-9-22
+ */
+#ifndef _NDROID_DARM_UTILS_H
+#define _NDROID_DARM_UTILS_H
+
+#ifdef __cplusplus
+
+extern "C"
+{
+#endif
+
+	typedef enum _darm_sr_type {
+		SRType_LSL,
+		SRType_LSR, 
+		SRType_ASR,
+		SRType_ROR,
+		SRType_RRX,
+		SRType_INVALID
+	} darm_sr_type;
+
+	int darm_shift(int value, int type, int imm, int carry_in);
+
+	darm_sr_type darm_decode_imm_shift(int type, int imm);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
